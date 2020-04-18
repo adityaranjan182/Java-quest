aa12378ed98c3c6729d0491228de0f7b44ef6cb1
Aditya Ranjan <aditya.mca19.du@gmail.com>
2020-04-18 23:16:19 +0530

    print the letter X according to given input value

--- xpattern.java
+++ xpattern.java
@@ -4,18 +4,18 @@ public class xpattern
     public static void main(String[] args) 
     {
         Scanner scn = new Scanner(System.in);
-        System.out.println("enter a number: ");
+        System.out.print("enter a number: ");
         int n = scn.nextInt();
         pattern(n);
         scn.close();    
     }
     public static void pattern(int n)
     {
-        for (int i = 1;i<=n;i++)
+        for (int i = 1;i<=2*n-1;i++)
         {
-            for (int j = 1; j<=n;j++)
+            for (int j = 1; j<=2*n-1;j++)
             {
-                if (i==j || i+j==n+1) 
+                if (i==j || i+j==2*n) 
                     System.out.print(" * ");
                 else
                     System.out.print("   ");
